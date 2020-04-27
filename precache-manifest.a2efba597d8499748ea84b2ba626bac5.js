041a1a42c321bdf962b66a5767fafa06f0ca7921
cold-pac <erol.a.cimen@gmail.com>
2020-04-27 23:21:02 +1000

    Updates

---
+++ precache-manifest.a2efba597d8499748ea84b2ba626bac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe590929db86eb27d2f13aead8d6e6f",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "e23237bf36f0a361bd03",
+    "url": "/markdown-previewer/static/css/main.f07f9cd0.chunk.css"
+  },
+  {
+    "revision": "9f532998c9c315315671",
+    "url": "/markdown-previewer/static/js/2.777de546.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/markdown-previewer/static/js/2.777de546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23237bf36f0a361bd03",
+    "url": "/markdown-previewer/static/js/main.309d296b.chunk.js"
+  },
+  {
+    "revision": "355c044fd08903e25d19",
+    "url": "/markdown-previewer/static/js/runtime-main.9d5d89ab.js"
+  }
+]);
